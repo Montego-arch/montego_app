4804dbc784c18dae1b965d5c5c8f0b06f4deb76c
Montego-arch <mmanuelmiles@gmail.com>
2023-10-07 13:47:08 +0100

    first commit

--- montego_app/hooks.py
+++ montego_app/hooks.py
@@ -14,7 +14,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/montego_app/css/montego_app.css"
-# app_include_js = "/assets/montego_app/js/montego_app.js"
+# app_include_js = "/assets/montego_app/js/montego_ap
 
 # include js, css files in header of web template
 # web_include_css = "/assets/montego_app/css/montego_app.css"
